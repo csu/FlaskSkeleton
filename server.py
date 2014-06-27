dc501ddcc7dc4a5dded5260ea7e9d3f47e0fcd85
Christopher Su <christophersu9@gmail.com>
2014-06-26 22:42:45 -0700

    reorganized bootstrap

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, jsonify, request
-import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.DATABASE_NAME
-collection = db.COLLECTION_NAME
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client.DATABASE_NAME
+# collection = db.COLLECTION_NAME
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({'status': 'success'})
+    return render_template('index.html', header='Flask Skeleton', title='Flask Skeleton', body='''
+        <p>Some content can go here!</p>
+        ''')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
