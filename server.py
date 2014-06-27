bf9f73908cc2b58e623e7a6ad4c4bb9ee684f610
Christopher Su <christophersu9@gmail.com>
2014-06-26 22:58:17 -0700

    final

--- server.py
+++ server.py
@@ -4,15 +4,14 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')  # todo: change to the Heroku environment variable that contains the path for the MongoLab database
-db = client.DATABASE_NAME
-collection = db.COLLECTION_NAME
+# client = MongoClient('mongodb://localhost:27017/')  # todo: change to the Heroku environment variable that contains the path for the MongoLab database
+# db = client.DATABASE_NAME
+# ollection = db.COLLECTION_NAME
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', header='Flask Skeleton', title='Flask Skeleton', body='''
         <p>Some content can go here!</p>
         ''')
-
 if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+    app.run(host='0.0.0.0', debug=True, port=33507)
